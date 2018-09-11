3ff64eb05f2782f88e7a06eb6804c044d9c88906
hanzhong1987@gmail.com <hanzhong1987@gmail.com>
2018-09-11 22:22:54 +0900

    source code modified

--- tools/trade_algotithms/seq2seq_model.py
+++ tools/trade_algotithms/seq2seq_model.py
@@ -14,25 +14,49 @@ import gpyopt_for_keras
 # website
 #https://github.com/farizrahman4u/seq2seq.git
 class simple_seq2seq():
-    def __init__(self):
+    def __init__(self,
+        input_dim,
+        input_length,
+        output_dim,
+        hidden_dim,
+        output_length,
+        depth,
+        batch_size,
+        epochs,
+        validation_split,
+        num_classes):
+        
         self.input_dim=None
+        self.input_length=input_length
         self.hidden_dim=None
         self.output_dim=None
         self.output_length=None
         self.depth=None
         self.loss_func_name="mse"
         self.optimizer_name="rmsprop"
-        self.gpyopt_bounds=None
-        self.gpyopt_bounds=None
         self.test_rate=0.2
+        #the followings are common part
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.validation_split = validation_split
+        self.num_classes=num_classes
+        self.baseSaveDir = "./simple_model_files/"
+        self.model_file=self.baseSaveDir+"model.hdf5"
+        self.model_arch=self.baseSaveDir+"model.json"
+        self.best_model=self.baseSaveDir+"best_model.hdf5"
+        self.best_arch=self.baseSaveDir+"best_model.json"
+
+    def create_model_folder(self):
+        if not os.path.isdir(self.baseSaveDir):
+            os.makedirs(self.baseSaveDir)
 
     def build_train_test_data(self,input_array,output_array,test_rate):
         input_shape=input_array.shape
         self.input_dim=input_shape[1]
         output_shape=output_array.shape
         self.output_dim=output_shape.shape[1]
-        
-    def model(self,input_dim,hidden_dim,output_length,output_dim,depth):
+
+    def build_model(self,input_dim,hidden_dim,output_length,output_dim,depth):
         #model = SimpleSeq2Seq(input_dim=, hidden_dim=10, output_length=8, output_dim=20, depth=(4, 5))
         self.model = SimpleSeq2Seq(input_dim=self.input_dim, hidden_dim=self.hidden_dim, output_length=self.output_length, output_dim=self.output_dim, depth=self.depth)
         self.model.compile(loss='mse', optimizer='rmsprop')
